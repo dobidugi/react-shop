02e0eee2653453a7b2bcbe33e56c5f38fe1c5941
dobidugi <dobidugi@gmail.com>
2022-04-29 13:49:43 +0900

    헤더 장바구니 카운터 Context State값 사용

--- src/components/Layout/Header/HeaderCartButton.jsx
+++ src/components/Layout/Header/HeaderCartButton.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../../store/cart-context";
+
 function HeaderCartButton(props) {
+    const ctx = useContext(CartContext);
+    const itemCount = ctx.items.length;
     return (
         <button {...props} className={classes.button}>
             <span className={classes.icon}><AddShoppingCartIcon /></span>
             <span>Your Cart</span>
-            <span className={classes.count}>3</span>
+            <span className={classes.count}>{itemCount}</span>
         </button>
     );
 }
--- src/store/CartContextProvider.jsx
+++ src/store/CartContextProvider.jsx
@@ -4,7 +4,7 @@ import CartContext from "./cart-context";
 function CartContextProvider(props) {
     const [items, setItems] = useState([]);
     const addItemToCartHandler = (id) => {
-        
+
     };
 
     const removeItemToCartHandler = (id) => {
@@ -17,7 +17,7 @@ function CartContextProvider(props) {
     };
     return (
         <CartContext.Provider value={cartContext}>
-            {props.chilren}
+            {props.children}
         </CartContext.Provider>
     );
 }
